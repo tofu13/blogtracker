b8db50e59b2954b718214238f02e16cfbec24283
marc8 <brtmrc@gmail.com>
2021-01-31 23:20:04 +0100

    Add initial db creation

--- main.py
+++ main.py
@@ -1,10 +1,25 @@
 from sanic import Sanic
 from sanic.response import empty, json, redirect
 import sqlite3
+from os.path import exists
+
+_create_db = False if exists("blogtracker.db") else True
+db = sqlite3.connect("blogtracker.db", isolation_level=None)  # None -> autocommit
+
+if _create_db:
+    db.execute("""
+    CREATE TABLE topic (name text not null primary key);
+    """)
+    db.execute("""
+    CREATE TABLE "track" (
+        id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+        date TEXT NOT NULL,
+        "text" TEXT NOT NULL,
+        topic text NOT NULL
+        );
+    """)
 
 app = Sanic(__name__)
-
-db = sqlite3.connect("blogtracker.db", isolation_level=None) # None -> autocommit
 app.static('/static', './static')
 
 
@@ -18,7 +33,7 @@ async def story(request):
     topic = request.args.get("topic")
     if topic:
         cursor = db.cursor()
-        cursor.execute("SELECT id, date, text FROM track WHERE topic = ? ORDER BY date DESC", [topic,])
+        cursor.execute("SELECT id, date, text FROM track WHERE topic = ? ORDER BY date DESC", [topic, ])
         return json(cursor.fetchall())
     else:
         return empty()
@@ -28,9 +43,9 @@ async def story(request):
 async def save(request):
     cursor = db.cursor()
     cursor.execute("INSERT INTO track(id,date,topic,text) VALUES (NULL,date('now'),?,'')",
-               (
-                   request.json["topic"],
-               ))
+                   (
+                       request.json["topic"],
+                   ))
     return json(cursor.lastrowid)
 
 
@@ -38,11 +53,11 @@ async def save(request):
 async def save(request, tracknumber):
     cursor = db.cursor()
     cursor.execute("UPDATE track SET date=?, text=? WHERE id=?",
-               (
-                   request.json["date"],
-                   request.json["text"],
-                   tracknumber
-               ))
+                   (
+                       request.json["date"],
+                       request.json["text"],
+                       tracknumber
+                   ))
     return empty()
 
 
@@ -50,9 +65,9 @@ async def save(request, tracknumber):
 async def delete(request, tracknumber):
     cursor = db.cursor()
     cursor.execute("DELETE FROM track WHERE id=?",
-               (
-                   tracknumber,
-               ))
+                   (
+                       tracknumber,
+                   ))
     return empty()
 
 
@@ -68,18 +83,18 @@ async def newtopic(request):
     cursor = db.cursor()
     try:
         cursor.execute("INSERT INTO topic(name) VALUES (?)",
-                   (
-                       request.json,
-                   ))
+                       (
+                           request.json,
+                       ))
     except sqlite3.Error as err:
         pass
     return empty()
 
 
-
 @app.route("/favicon.ico")
 async def favicon(request):
     return empty()
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8000)
